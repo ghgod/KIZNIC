ea8d1423c3c032cd8da5bb89f4773aa7573e2311
hong <aiaipming@gmail.com>
2014-05-13 02:24:48 +0900

    fragment dialog 적용 및 title icon 교체

--- Kiznic/src/com/hhh/kiznic/SearchcategoryDialog.java
+++ Kiznic/src/com/hhh/kiznic/SearchcategoryDialog.java
@@ -4,11 +4,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,70 +22,66 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class SearchcategoryDialog extends Dialog {
+@SuppressLint("ValidFragment")
+public class SearchcategoryDialog extends DialogFragment {
 
-	private Context context;
+	Context context;
 	
-	private TextView dialog_title_textview;
-	private ListView dialog_firstlist_listview;
-	private ListView dialog_secondlist_listview;
+	TextView dialog_title_textview;
+	ListView dialog_firstlist_listview;
+	ListView dialog_secondlist_listview;
 	
-	private String dialog_title;
-	private String[] list_item;
-	private String[] list_item2;
+	String dialog_title;
 	
-	private AdapterView.OnItemClickListener firstlistClickListener;
-	private AdapterView.OnItemClickListener secondlistClickListener;
+	ArrayAdapter<String> first_arrayAdapter = null;
+	ArrayAdapter<String> second_arrayAdapter = null;
+	
+	AdapterView.OnItemClickListener firstlistClickListener = null;
+	AdapterView.OnItemClickListener secondlistClickListener = null;
+	
+	Dialog dialog;
+	
+	interface onSubmitListener{
+		void setListener(String arg);
+	}
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	public Dialog onCreateDialog(Bundle savedInstanceState){
+		dialog = new Dialog(getActivity());
 		
-		WindowManager.LayoutParams lpWindow = new WindowManager.LayoutParams();    
-		lpWindow.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
-		lpWindow.dimAmount = 0.8f;
-		getWindow().setAttributes(lpWindow);
-		
+		dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);  
+		dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); 
+	
 		if(secondlistClickListener == null)
-			setContentView(R.layout.dailog_search);
+			dialog.setContentView(R.layout.dailog_search);
 		else
-			setContentView(R.layout.dailog_search2);
+			dialog.setContentView(R.layout.dailog_search2);
+	
+		//dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+		
 		init();
-		setTitle();
-		if(secondlistClickListener == null)
-			setList(dialog_firstlist_listview);
-		else
-			setList(dialog_firstlist_listview, dialog_secondlist_listview);
+		
+		dialog_title_textview.setText(dialog_title);
+		
+		setList();
 		
 		setClickListener(firstlistClickListener , secondlistClickListener);
-	}
-	
-	public void init(){
-		dialog_title_textview = (TextView)findViewById(R.id.search_dailog_title_text);
-		if(secondlistClickListener == null)
-			dialog_firstlist_listview = (ListView)findViewById(R.id.search_dialog_list_list);
-		else{
-			dialog_firstlist_listview = (ListView)findViewById(R.id.search_dialog_firstlist_list);
-			dialog_secondlist_listview = (ListView)findViewById(R.id.search_dialog_secondlist_list);
-		}
-	}
-	
-	public SearchcategoryDialog(Context context , String title , 
-			AdapterView.OnItemClickListener singleListener) {
-		super(context , android.R.style.Theme_Translucent_NoTitleBar);
-		this.context = context;
-		dialog_title = title;
-		firstlistClickListener = singleListener;
-	}
-	
-	public SearchcategoryDialog(Context context , String title , 
-			AdapterView.OnItemClickListener firstListener, AdapterView.OnItemClickListener secondListener) {
-		super(context , android.R.style.Theme_Translucent_NoTitleBar);
-		this.context = context;
-		dialog_title = title;
-		firstlistClickListener = firstListener;
-		secondlistClickListener = secondListener;
 		
+		dialog.show();
+		
+		return dialog;
+	}
+
+	public void init(){
+		dialog_title_textview = (TextView)dialog.findViewById(R.id.search_dailog_title_text);
+		
+		if(second_arrayAdapter != null){
+			dialog_firstlist_listview = (ListView)dialog.findViewById(R.id.search_dialog_firstlist_list);
+			dialog_secondlist_listview = (ListView)dialog.findViewById(R.id.search_dialog_secondlist_list);
+		}
+		else{
+			dialog_firstlist_listview = (ListView)dialog.findViewById(R.id.search_dialog_list_list);
+		}
 	}
 	
 	private void setClickListener(AdapterView.OnItemClickListener first , AdapterView.OnItemClickListener second){
@@ -92,39 +94,12 @@ public class SearchcategoryDialog extends Dialog {
 		}
 	}
 	
-	private void setTitle(){
-		dialog_title_textview.setText(dialog_title);
-	}
-	
-private void setList(ListView dialoglist){
-		
+	private void setList(){
+			
 		//if(dialog_title.equals("장르"))
-			list_item = context.getResources().getStringArray(R.array.dialog_list_genre);
+		dialog_firstlist_listview.setAdapter(first_arrayAdapter);
 		
-		List<String> listItem = Arrays.asList(list_item);
-		ArrayList<String> itemArrayList = new ArrayList<String> (listItem);
-		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, itemArrayList);
-		dialog_firstlist_listview.setAdapter(arrayAdapter);
-	
-		//dialog_list_listview.getChildAt(0).setBottom(10);
+		if(second_arrayAdapter != null)
+			dialog_secondlist_listview.setAdapter(second_arrayAdapter);
 	}
-
-private void setList(ListView firstdialoglist, ListView seconddialoglist){
-	
-	list_item = context.getResources().getStringArray(R.array.dialog_list_genre);
-	
-	List<String> listItem = Arrays.asList(list_item);
-	ArrayList<String> itemArrayList = new ArrayList<String> (listItem);
-	ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, itemArrayList);
-	dialog_firstlist_listview.setAdapter(arrayAdapter);
-
-	//dialog_list_listview.getChildAt(0).setBottom(10);
-	
-	list_item2 = context.getResources().getStringArray(R.array.dialog_list_genre);
-	
-	List<String> listItem2 = Arrays.asList(list_item);
-	ArrayList<String> itemArrayList2 = new ArrayList<String> (listItem2);
-	ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, itemArrayList2);
-	dialog_secondlist_listview.setAdapter(arrayAdapter2);
-}
 }
